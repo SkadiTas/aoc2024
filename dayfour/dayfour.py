eba02092b2fd22629338ca79c2b4216595ba6cbe
Alexander Levett <alevett@nexigen.digital>
2024-12-09 09:50:12 +1100

    Make import not dumb

--- dayfour/dayfour.py
+++ dayfour/dayfour.py
@@ -1,25 +1,4 @@
 # Data Import #
-data = open("./input.txt", "r")
-rows = []
+wordsearch = open("./input.txt", "r").read().splitlines()
 
-for line in data:
-    parsed = filter(None, line.split("\n"))
-    rows.append(list(parsed))
-
-# Grid-ify #
-grid = []
-for i in range(len(rows)):
-    grid.append([list(row) for row in rows[i]])
-
-
-#def findNextLetter(grid, ypos, xpos, goal):
-#something something
-
-currentLetter = None
-foundCount = 0
-
-for i in range(len(grid)):
-    for j in range(len(grid[i])):
-        for k in range(len(grid[i][j])):
-            currentLetter = grid[i][j][k]
 
