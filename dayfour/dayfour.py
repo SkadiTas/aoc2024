6d78a71417a43b26c7d9279e67e5084dc8b59364
Alexander Levett <alevett@nexigen.digital>
2024-12-09 14:08:43 +1100

    Finished Part Two

--- dayfour/dayfour.py
+++ dayfour/dayfour.py
@@ -1,6 +1,8 @@
 # Data Import #
 wordsearch = open("./input.txt", "r").read().splitlines()
 
+# ------------- Part One -----------------#
+
 #Neighbour Coordinates (defined as Y, X)
 neighbours = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 0], [0, 1], [1, -1], [1, 0], [1, 1]]
 
@@ -33,6 +35,55 @@ def findXmas():
                                     
 
 partOneAnswer = findXmas()
-print("Found: ",partOneAnswer)
+print("Part One Answer: ",partOneAnswer)
 
-#only use existing neighbour to maintain direction
+# ------------- Part Two -----------------#
+
+p2neighbours = [[-1, -1], [-1, 1], [1, -1], [1, 1]]
+p2opposites = [[1, 1], [1, -1], [-1, 1], [-1, -1]]
+
+def findStupidXmas():
+    totalStupidFound = 0
+
+    for i in range(len(wordsearch)):
+        for j in range(len(wordsearch[i])):
+            if wordsearch[i][j] == 'A':
+                # Found A, consult neighbours for M
+                for k in range(len(p2neighbours)):
+                    checkY = i + (p2neighbours[k][1])
+                    checkX = j + (p2neighbours[k][0])
+                    unconsumed = []
+                    
+                    if (-1 < checkY < len(wordsearch) and -1 < checkX < len(wordsearch[i])):
+                        if (wordsearch[checkY][checkX] == 'M'):
+                            # Found M, check defined opposite for S
+                            checkY = i + (p2opposites[k][1])
+                            checkX = j + (p2opposites[k][0])
+                
+                            if (-1 < checkY < len(wordsearch) and -1 < checkX < len(wordsearch[i])):
+                                if (wordsearch[checkY][checkX] == 'S'):
+                                    #Found Diagonal MAS, initialise unconsumed coordinates
+
+                                    for coords in p2neighbours:
+                                        if coords != p2neighbours[k] and coords != p2opposites[k]:
+                                            unconsumed.append(coords)
+
+                                    #Check for MAS in unconsumed
+                                    for n in range(len(unconsumed)):
+                                        checkY = i + (unconsumed[n][1])
+                                        checkX = j + (unconsumed[n][0])
+                                        
+                                        if (-1 < checkY < len(wordsearch) and -1 < checkX < len(wordsearch[i])):
+                                            if (wordsearch[checkY][checkX] == 'M'):
+                                                # Found M, check defined opposite for S
+                                                for m in range(len(unconsumed)):
+                                                    checkY = i + (unconsumed[m][1])
+                                                    checkX = j + (unconsumed[m][0])
+
+                                                    if (-1 < checkY < len(wordsearch) and -1 < checkX < len(wordsearch[i])):
+                                                        if (wordsearch[checkY][checkX] == 'S'):
+                                                            totalStupidFound += 1
+    return totalStupidFound
+
+partTwoAnswer = findStupidXmas()
+print("Part Two Answer: ",(partTwoAnswer/2))
