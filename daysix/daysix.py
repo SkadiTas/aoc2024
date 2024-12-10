721c8bacb11f0075d82b7ceec17883d3c055042d
Alexander Levett <82991691+SkadiTas@users.noreply.github.com>
2024-12-10 17:44:36 +1100

    made it work and not that stupid

--- daysix/daysix.py
+++ daysix/daysix.py
@@ -2,6 +2,12 @@
 data = open("./input.txt", "r").read().splitlines()
 
 guards = ['^', '>', 'v', '<']
+# Create traversal history #
+map = []
+for i in range(len(data)):
+    map.append([])
+    for j in range(len(data[i])):
+        map[i].append(0)
 
 #guard[0] = row/y-pos
 #guard[1] = column/x-pos
@@ -16,6 +22,7 @@ def findGuard():
                 currentY = y
                 currentX = x
                 direction = guards.index(data[y][x])
+                map[y][x] = 1
     return [currentY, currentX, direction, 1, True]
 
 def goDir(guard):
@@ -24,8 +31,6 @@ def goDir(guard):
         #use the same x coordinate, check decreasing y coordinate until obstacle or edge
         for y in reversed(range(0, guard[0])):
             if data[y][guard[1]] == '#':
-                dist = guard[0] - y
-                guard[3] += (dist-1)
                 guard[0] = y+1
                 if guard[2] < 3:
                     guard[2] += 1
@@ -33,22 +38,17 @@ def goDir(guard):
                     guard[2] = 0
                 return guard
             elif data[y][guard[1]] in ['.', '^']:
+                map[y][guard[1]] = 1
                 if y == 0:
                     print("Bounds reached, guard exiting")
-                    dist = guard[0]
-                    guard[3] += dist
                     guard[4] = False
                     return guard
-                else:
-                    continue
 
     elif guard[2] == 1:
         print("Right")
         #use the same y coordinate, check increasing x coordinate until obstance or edge
         for x in range(guard[1], len(data[0])):
             if data[guard[0]][x] == '#':
-                dist = x - guard[1]
-                guard[3] += (dist-1)
                 guard[1] = x-1
                 if guard[2] < 3:
                     guard[2] += 1
@@ -56,22 +56,17 @@ def goDir(guard):
                     guard[2] = 0
                 return guard
             elif data[guard[0]][x] in ['.', '^']:
+                map[guard[0]][x] = 1
                 if x == len(data[0])-1:
                     print("Bounds reached, guard exiting")
-                    dist = len(data[0]) - guard[1]
-                    guard[3] += dist
                     guard[4] = False
                     return guard
-                else:
-                    continue
 
     elif guard[2] == 2:
         print("Down")
         #use the same x coordinate, check increasing y coordinate until obstacle or edge
         for y in range(guard[0], len(data)):
             if data[y][guard[1]] == '#':
-                dist = y - guard[0]
-                guard[3] += (dist-1)
                 guard[0] = y-1
                 if guard[2] < 3:
                     guard[2] += 1
@@ -79,22 +74,17 @@ def goDir(guard):
                     guard[2] = 0
                 return guard
             elif data[y][guard[1]] in ['.', '^']:
+                map[y][guard[1]] = 1
                 if y == len(data)-1:
                     print("Bounds reached, guard exiting")
-                    dist = len(data) - guard[0]
-                    guard[3] += dist
                     guard[4] = False
                     return guard
-                else:
-                    continue
 
     elif guard[2] == 3:
         print("Left")
         #use the same y coodinate, check decreasing x coordinate until obstacle or edge
-        for x in reversed(range(0, len(data[0]))):
+        for x in reversed(range(0, guard[1])):
             if data[guard[0]][x] == '#':
-                dist = guard[1] - x
-                guard[3] += (dist-1)
                 guard[1] = x+1
                 if guard[2] < 3:
                     guard[2] += 1
@@ -102,21 +92,21 @@ def goDir(guard):
                     guard[2] = 0
                 return guard
             elif data[guard[0]][x] in ['.', '^']:
+                map[guard[0]][x] = 1
                 if x == 0:
                     print("Bounds reached, guard exiting")
-                    dist = guard[1]
-                    guard[3] += dist
                     guard[4] = False
                     return guard
-                else:
-                    continue
     return guard
 
 guard = findGuard()
-print("Initial Guard: ",guard)
 
 while guard[4] == True:
     guard = goDir(guard)
-    
-print("Patrol complete, traversed distance: ", guard[3])
-print("Guard Position: ",guard[0],guard[1])
+
+traversed = 0
+for row in map:
+    for num in row:
+        if num == 1:
+            traversed += 1
+print(traversed)
