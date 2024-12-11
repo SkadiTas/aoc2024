e2c5b383a525719e6e0e0986a43446b483f9c001
Alexander Levett <alevett@nexigen.digital>
2024-12-12 10:55:40 +1100

    Finished day 7

--- dayseven/dayseven.py
+++ dayseven/dayseven.py
@@ -1,7 +1,7 @@
 import itertools
 # Import data and format #
 data = open("./input.txt", "r").read().splitlines()
-operators = ['+', '*']
+operators = ['+', '*', '||']
 formatted = []
 vals = []
 sums = []
@@ -17,9 +17,12 @@ for row in formatted:
     lsum = [int(n) for n in stripped.split(' ')]
     sums.append(lsum)
 
-# ------------ Part One ---------- #
+
 def evaluate(num1, num2, operator):
-    return eval(f"{num1} {operator} {num2}")
+    if operator in ['*','+']:
+        return eval(f"{num1} {operator} {num2}")
+    else: 
+        return int(str(num1) + str(num2))
 
 for sublist in sums:
     goal = vals[sums.index(sublist)]
@@ -33,13 +36,15 @@ for sublist in sums:
             for i, op in enumerate(combination):
                 result = evaluate(result, sublist[i+1], op)
 
-                if result == goal:
-                    print("Successfully found: ", goal)
-                    p1 += result
-                    found = True
-                    break
+            if result == goal:
+                print("Successfully found: ", goal)
+                p1 += result
+                found = True
+                break
         else:
             break
 
 print(p1)
-# ------------ End Part One ---------- #
+
+#Completed part 1 and part 2 (combined)
+# To revert to p1 just remove the concat operator from the list of operators at the top
--- daysix/daysixp2.py
+++ daysix/daysixp2.py
@@ -1,114 +1,41 @@
-# Import data and format #
-data = open("./input.txt", "r").read().splitlines()
+# Input data and formatting #
+data = open("./input.txt", "r").read().strip()
 
-guards = ['^', '>', 'v', '<']
-# Create traversal history #
-map = []
-for i in range(len(data)):
-    map.append([])
-    for j in range(len(data[i])):
-        map[i].append(0)
+grid = data.split('\n')
+rows = len(grid)
+columns = len(grid[0])
 
-#guard[0] = row/y-pos
-#guard[1] = column/x-pos
-#guard[2] = direction
-#guard[3] = traversed
-#guard[4] = Is the guard still on the map?
-def findGuard():
-    for y in range(len(data)):
-        for x in range(len(data[y])):
-            #Find guard
-            if data[y][x] in guards:
-                currentY = y
-                currentX = x
-                direction = guards.index(data[y][x])
-                map[y][x] += 1
-    return [currentY, currentX, direction, 1, True]
+p1 = 0
+p2 = 0
 
-def goDir(guard):
-    if guard[2] == 0:
-        print("Up")
-        #use the same x coordinate, check decreasing y coordinate until obstacle or edge
-        for y in reversed(range(0, guard[0])):
-            if data[y][guard[1]] == '#':
-                guard[0] = y+1
-                if guard[2] < 3:
-                    guard[2] += 1
-                else:
-                    guard[2] = 0
-                return guard
-            elif data[y][guard[1]] in ['.', '^']:
-                map[y][guard[1]] += 1
-                if y == 0:
-                    print("Bounds reached, guard exiting")
-                    guard[4] = False
-                    return guard
+for r in range(rows):
+    for c in range(columns):
+        if grid[r][c] == '^':
+            startrow,startcol = r,c
 
-    elif guard[2] == 1:
-        print("Right")
-        #use the same y coordinate, check increasing x coordinate until obstance or edge
-        for x in range(guard[1], len(data[0])):
-            if data[guard[0]][x] == '#':
-                guard[1] = x-1
-                if guard[2] < 3:
-                    guard[2] += 1
-                else:
-                    guard[2] = 0
-                return guard
-            elif data[guard[0]][x] in ['.', '^']:
-                map[guard[0]][x] += 1
-                if x == len(data[0])-1:
-                    print("Bounds reached, guard exiting")
-                    guard[4] = False
-                    return guard
-
-    elif guard[2] == 2:
-        print("Down")
-        #use the same x coordinate, check increasing y coordinate until obstacle or edge
-        for y in range(guard[0], len(data)):
-            if data[y][guard[1]] == '#':
-                guard[0] = y-1
-                if guard[2] < 3:
-                    guard[2] += 1
-                else:
-                    guard[2] = 0
-                return guard
-            elif data[y][guard[1]] in ['.', '^']:
-                map[y][guard[1]] += 1
-                if y == len(data)-1:
-                    print("Bounds reached, guard exiting")
-                    guard[4] = False
-                    return guard
-
-    elif guard[2] == 3:
-        print("Left")
-        #use the same y coodinate, check decreasing x coordinate until obstacle or edge
-        for x in reversed(range(0, guard[1])):
-            if data[guard[0]][x] == '#':
-                guard[1] = x+1
-                if guard[2] < 3:
-                    guard[2] += 1
-                else:
-                    guard[2] = 0
-                return guard
-            elif data[guard[0]][x] in ['.', '^']:
-                map[guard[0]][x] += 1
-                if x == 0:
-                    print("Bounds reached, guard exiting")
-                    guard[4] = False
-                    return guard
-    return guard
-
-guard = findGuard()
-
-while guard[4] == True:
-    guard = goDir(guard)
-
-repeats = []
-for y in range(len(map)):
-    for x in range(len(map[y])):
-        if map[y][x] > 1:
-            repeats.append((y, x))
-
-for row in repeats:
-    print(row)
+for nrow in range(rows):
+    for ncol in range(columns):
+        r,c = startrow,startcol
+        d = 0 #0 up 1 right 2 down 3 left
+        seen = set()
+        seendir = set()
+        while True:
+            if (r,c,d) in seendir:
+                p2 += 1
+                break
+            seendir.add((r,c,d))
+            seen.add((r,c))
+            dr,dc = [(-1,0),(0,1),(1,0),(0,-1)][d]
+            rr = r+dr
+            cc = c+dc
+            if not (0<=rr<rows and 0<=cc<columns):
+                if grid[nrow][ncol]=='#':
+                    p1 = len(seen)
+                break
+            if grid[rr][cc]=='#' or rr==nrow and cc==ncol:
+                d = (d+1)%4
+            else:
+                r = rr
+                c = cc
+print("Part One Answer: ",p1)
+print("Part Two Answer: ",p2)
