0921a839159dfd23f129b37ce35d0da0c5cfb880
Alexander Levett <alevett@nexigen.digital>
2024-12-16 12:42:43 +1100

    Day 13 Complete

--- daythirteen/daythirteen.py
+++ daythirteen/daythirteen.py
@@ -1,4 +1,5 @@
-import re, sys, time
+import re, sys, sympy as sp
+
 data = open("./input.txt", "r").read()
 
 pattern = (
@@ -18,29 +19,28 @@ sum_y = [int(match[5]) for match in matches]
 sum_x_pt2 = [(int(match[4]) + 10000000000000) for match in matches]
 sum_y_pt2 = [(int(match[5]) + 10000000000000) for match in matches]
 
-def sumArrays(a, b):
-    sum = []
-    for i in range(len(a)):
-        sum.append(a[i] + b[i])
-    return sum
+def evaluatePrize(adata, bdata, p):
+    a, b = sp.symbols('a b')
+    #unpack the bunttons n prize
+    ax, ay = adata[0], adata[1] 
+    bx, by = bdata[0], bdata[1]
 
+    prizex, prizey = p[0], p[1]
 
-def evaluatePrize(a, b, p, loops):
-    cost = 0
-    for i in range(1, loops):
-        sa = [c * i for c in a]
-        for j in range(1, loops):
-            sb = [c * j for c in b]
-            t = sumArrays(sa, sb)
-            if t == p:
-                if cost == 0:
-                    #first found result
-                    cost = ((3*i) + j)
-                elif ((3*a)+b) < cost:
-                    cost = ((3*i) + j)
-            if t > p:
-                break
-    return cost
+    eq1 = sp.Eq(a * ax + b * bx, prizex)
+    eq2 = sp.Eq(a * ay + b * by, prizey)
+
+    solution = sp.solve((eq1, eq2), (a, b))
+
+    if solution:
+        founda = solution[a]
+        foundb = solution[b]
+        if founda.is_integer and foundb.is_integer:
+            return ((founda * 3) + (foundb))
+        else:
+            return 0
+    else:
+        return 0
 
 # Part One #
 totalCost = 0
@@ -48,18 +48,16 @@ for i in range(len(matches)):
     btnA = [ax[i], ay[i]]
     btnB = [bx[i], by[i]]
     prize = [sum_x[i], sum_y[i]]
-    totalCost += evaluatePrize(btnA, btnB, prize, 100)
-print("Total Cost For All Possible Prizes (Part One): ", totalCost)
+    totalCost += evaluatePrize(btnA, btnB, prize)
+    sys.stdout.write("\r(Part One) Current Game: %i | Current Cost: %i" %(i, totalCost))
+print("\nTotal Cost For All Possible Prizes (Part One): ", totalCost)
 
 # Part Two #
 totalCost = 0
-maxgoal = (max(max(sum_x_pt2), max(sum_y_pt2)))
-minmultiple = int(min(min(ax), min(ay), min(bx), min(by)))
-loopMax = round(maxgoal / minmultiple)
-
 for i in range(len(matches)):
-    btnA = [ax[i], ay[i]]
-    btnB = [bx[i], by[i]]
-    prize = [sum_x_pt2[i], sum_y_pt2[i]]
-    totalCost += evaluatePrize(btnA, btnB, prize, 100)
-print("Total Cost For All Possible Prizes (Part Two): ", totalCost)
+    btnA = (ax[i], ay[i])
+    btnB = (bx[i], by[i])
+    prize = (sum_x_pt2[i], sum_y_pt2[i])
+    totalCost += evaluatePrize(btnA, btnB, prize)
+    sys.stdout.write("\r(Part Two) Current Game: %i | Current Cost: %i" %(i, totalCost))
+print("\nTotal Cost For All Possible Prizes (Part Two): ", totalCost)
